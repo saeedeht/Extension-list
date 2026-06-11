bfe9764cf76affa069e4a306f14ced447c34c1af
miladbakhtazad <hanaajorloo@gmail.com>
2026-06-11 19:02:20 +0330

    header

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -1,7 +1,10 @@
 export const TopHeader = () => {
   return (
-    <>
-      <h1>TopHeader</h1>
-    </>
+    <div className="w-full h-20 flex justify-between items-center  ">
+      <h2 className="text-2xl font-extrabold">Extensions List (12)</h2>
+      <div className="flex justify-center items-center gap-5 ">
+        <div className="bg-red-500"></div>
+      </div>
+    </div>
   );
 };
